ec3494800b04fb45cf34674b63fb5c2b443b6c80
baekseungho <vudrkd1580@naver.com>
2025-05-31 14:00:32 +0900

    25.05.31 qna로직완성, 월별데이터 버그수정

--- models/QnA.js
+++ models/QnA.js
@@ -2,19 +2,29 @@
 const mongoose = require("mongoose");
 
 const qnaSchema = new mongoose.Schema(
-    {
-        title: { type: String, required: true },
-        category: { type: String, required: true },
-        content: { type: String, required: true },
-        userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-        orderId: { type: mongoose.Schema.Types.ObjectId, ref: "Order" },
-        productName: { type: String },
-        imagePath: { type: String },
-        views: { type: Number, default: 0 },
+  {
+    title: { type: String, required: true },
+    category: { type: String, required: true },
+    content: { type: String, required: true },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    {
-        timestamps: true, // createdAt, updatedAt 자동 생성
-    }
+    orderId: { type: mongoose.Schema.Types.ObjectId, ref: "Order" },
+    productName: { type: String },
+    imagePath: { type: String },
+    views: { type: Number, default: 0 },
+    answer: {
+      content: String,
+      createdAt: { type: Date, default: Date.now },
+      adminId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    },
+  },
+
+  {
+    timestamps: true, // createdAt, updatedAt 자동 생성
+  }
 );
 
 module.exports = mongoose.model("QnA", qnaSchema);
