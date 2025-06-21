2d7119759f85565f57745cf658cf0192a1077155
baekseungho <vudrkd1580@naver.com>
2025-06-21 12:20:27 +0900

    25.06.21 상품,키트번호추가

--- models/Kit.js
+++ models/Kit.js
@@ -1,29 +1,33 @@
 const mongoose = require("mongoose");
 
 const kitSchema = new mongoose.Schema({
-    kitName: { type: String, required: true },
-    products: [
-        {
-            productId: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "Product",
-                required: true,
-            },
-            quantity: { type: Number, required: true, default: 1 },
-        },
-    ],
-    price: { type: Number, required: true }, // 할인가격
-    originalPrice: { type: Number, required: true }, // 원래 총합 가격
-    description: { type: String },
-    createdAt: { type: Date, default: Date.now },
-    imagePath: {
-        type: String,
-        default: "/img/default_product.png",
+  kitCode: {
+    type: String,
+    unique: true,
+  },
+  kitName: { type: String, required: true },
+  products: [
+    {
+      productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+      quantity: { type: Number, required: true, default: 1 },
     },
-    detailImage: {
-        type: String,
-        default: "/img/default_detail.jpg",
-    },
+  ],
+  price: { type: Number, required: true }, // 할인가격
+  originalPrice: { type: Number, required: true }, // 원래 총합 가격
+  description: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  imagePath: {
+    type: String,
+    default: "/img/default_product.png",
+  },
+  detailImage: {
+    type: String,
+    default: "/img/default_detail.jpg",
+  },
 });
 
 module.exports = mongoose.model("Kit", kitSchema);
--- models/Product.js
+++ models/Product.js
@@ -1,49 +1,53 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    category: {
-        type: String,
-        required: true,
-    },
-    productName: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    koreanName: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    volume: {
-        type: Number,
-        required: true,
-    },
-    consumerPrice: {
-        type: Number,
-        required: true,
-    },
-    memberPrice: {
-        type: Number,
-        default: 0,
-    },
-    imagePath: {
-        type: String,
-        default: "/img/default_product.png",
-    },
-    detailImage: {
-        type: String,
-        default: "/img/default_detail.jpg",
-    },
-    stock: {
-        type: Number,
-        required: true,
-        default: 0,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
+  category: {
+    type: String,
+    required: true,
+  },
+  productCode: {
+    type: String,
+    unique: true,
+  },
+  productName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  koreanName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  volume: {
+    type: Number,
+    required: true,
+  },
+  consumerPrice: {
+    type: Number,
+    required: true,
+  },
+  memberPrice: {
+    type: Number,
+    default: 0,
+  },
+  imagePath: {
+    type: String,
+    default: "/img/default_product.png",
+  },
+  detailImage: {
+    type: String,
+    default: "/img/default_detail.jpg",
+  },
+  stock: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("Product", productSchema);
